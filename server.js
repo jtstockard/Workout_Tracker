86e183859f95cc69b00c2d191340ef9870de0c5a
jtstockard <jtstockard92@gmail.com>
2021-07-21 15:24:07 -0500

    added models and routes for db

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+
+const apiRoutes = require(`./routes/apiRoutes`);
+const htmlRoutes = require(`./routes/htmlRoutes`);
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger(`dev`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+apiRoutes(app);
+htmlRoutes(app);
+
+let MONGODB_URI = process.env.MONGODB_CONNECTION || `mongodb://localhost/workout`;
+
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
