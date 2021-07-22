a038b21687eee30c2ff58dd791c1040461a7a1b3
jtstockard <jtstockard92@gmail.com>
2021-07-22 13:33:31 -0500

    deploying

--- server.js
+++ server.js
@@ -17,9 +17,12 @@ app.use(express.static(`public`));
 apiRoutes(app);
 htmlRoutes(app);
 
-let MONGODB_URI = process.env.MONGODB_CONNECTION || `mongodb://localhost/workout`;
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
